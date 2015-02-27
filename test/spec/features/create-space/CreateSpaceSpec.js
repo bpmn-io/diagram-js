5233459a2c454ebd6504e004895a6abb4a20bfac
Frederik Berg <allyouneedis@gmail.com>
2015-02-27 14:12:07 +0100

    implemented vertical create-space functionality

--- test/spec/features/create-space/CreateSpaceSpec.js
+++ test/spec/features/create-space/CreateSpaceSpec.js
@@ -3,8 +3,8 @@ var TestHelper = require('../../../TestHelper'),
 
 /* global bootstrapDiagram, inject */
 
-var createSpace =require('../../../../lib/features/create-space');
-var modelingModule = require('../../../../lib/features/modeling'),
+var createSpace =require('../../../../lib/features/create-space'),
+    modelingModule = require('../../../../lib/features/modeling'),
     moveModule = require('../../../../lib/features/move');
 //    rulesModule = require('./rules');
 
@@ -13,10 +13,9 @@ describe('features/create-space - CreateSpace', function() {
 
   beforeEach(bootstrapDiagram({ modules: [ modelingModule, moveModule, createSpace ] }));
 
+  var rootShape, parentShape, parentShape2, parentShape3, parentShape4, parentShape5, parentShape6, parentShape7, childShape, childShape2, connection;
 
-  var rootShape, parentShape, parentShape2, parentShape3, childShape, childShape2, connection;
-
-  beforeEach(inject(function(elementFactory, canvas) {
+  beforeEach(inject(function(elementFactory, canvas, modeling, dragging) {
 
     rootShape = elementFactory.createRoot({
       id: 'root'
@@ -26,24 +25,52 @@ describe('features/create-space - CreateSpace', function() {
 
     parentShape = elementFactory.createShape({
       id: 'parent',
-      x: 500, y: 100, width: 300, height: 300
+      x: 500, y: 100, width: 310, height: 150
     });
 
     canvas.addShape(parentShape, rootShape);
 
     parentShape2 = elementFactory.createShape({
       id: 'parent2',
-      x: 400, y: 100, width: 15, height: 300
+      x: 400, y: 100, width: 15, height: 150
     });
 
     canvas.addShape(parentShape2, rootShape);
 
     parentShape3 = elementFactory.createShape({
       id: 'parent3',
-      x: 900, y: 100, width: 300, height: 300
+      x: 900, y: 100, width: 60, height: 150
     });
 
     canvas.addShape(parentShape3, rootShape);
+
+    parentShape4 = elementFactory.createShape({
+      id: 'parent4',
+      x: 100, y: 300, width: 60, height: 60
+    });
+
+    canvas.addShape(parentShape4, rootShape);
+
+    parentShape5 = elementFactory.createShape({
+      id: 'parent5',
+      x: 200, y: 350, width: 60, height: 60
+    });
+
+    canvas.addShape(parentShape5, rootShape);
+
+    parentShape6 = elementFactory.createShape({
+      id: 'parent6',
+      x: 300, y: 400, width: 60, height: 60
+    });
+
+    canvas.addShape(parentShape6, rootShape);
+
+    parentShape7 = elementFactory.createShape({
+      id: 'parent7',
+      x: 500, y: 410, width: 60, height: 60
+    });
+
+    canvas.addShape(parentShape7, rootShape);
 
     childShape = elementFactory.createShape({
       id: 'child',
@@ -54,19 +81,24 @@ describe('features/create-space - CreateSpace', function() {
 
     childShape2 = elementFactory.createShape({
       id: 'child2',
-      x: 600, y: 110, width: 100, height: 100
+      x: 700, y: 110, width: 100, height: 100
     });
-
-    canvas.addShape(childShape2, parentShape);
 
     connection = elementFactory.createConnection({
       id: 'connection',
-      waypoints: [ { x: 550, y: 210 }, { x: 550, y: 250 }, { x: 650, y: 250 }, { x: 650, y: 210 } ],
+      waypoints: [ { x: 150, y: 150 }, { x: 150, y: 200 }, { x: 350, y: 150 } ],
       source: childShape,
       target: childShape2
     });
 
     canvas.addConnection(connection, parentShape);
+
+    canvas.addShape(childShape2, parentShape);
+    modeling.connect(parentShape, parentShape4);
+    modeling.connect(parentShape, parentShape5);
+    modeling.connect(parentShape, parentShape6);
+    modeling.connect(parentShape, parentShape7);
+
   }));
 
   describe('create-space test',function(){
@@ -82,11 +114,10 @@ describe('features/create-space - CreateSpace', function() {
 
       //then
       elementRegistry.filter(function(element){
-        expect( !element.x || (element.x >= 400 || element.x <= 900) ).toBe(true);
+        expect( !element.x || element.x >= 400 || element.x <= 900 ).toBe(true);
       });
 
     }));
-
 
     iit('should move elements rightwards to create some space', inject(function(dragging, elementRegistry, createSpace) {
       //when
@@ -96,9 +127,24 @@ describe('features/create-space - CreateSpace', function() {
 
       //then
       elementRegistry.filter(function(element){
-        expect( !element.x || (element.x >= 600 || element.x <= 410) ).toBe(true);
+        expect( !element.x || element.x >= 600 || element.x <= 410 ).toBe(true);
       });
     }));
+
+    iit('should move elements then undo and finally have elements in the original place',
+      inject(function(dragging, elementRegistry, createSpace, commandStack
+    ) {
+      //when
+      dragging.activate(createEvent({ x: 400, y: 10 }), 'create-space');
+      dragging.move(createEvent({ x: 900, y: 11 }));
+      dragging.end();
+      commandStack.undo();
+
+      //then
+      console.log(parentShape7);
+      expect(parentShape7.x === 500 && parentShape7.y === 410).toBe(true);
+    }));
+
 
     iit('should move elements leftwards to create some space', inject(function(dragging, elementRegistry, createSpace) {
       //when
@@ -108,11 +154,33 @@ describe('features/create-space - CreateSpace', function() {
 
       //then
       elementRegistry.filter(function(element){
-        expect( !element.x || (element.x >= 710 || element.x <= 510) ).toBe(true);
+        expect( !element.x || element.x >= 710 || element.x <= 510 ).toBe(true);
       });
     }));
 
+    iit('should move elements downwards to create some space', inject(function(dragging, elementRegistry, createSpace) {
+      //when
+      dragging.activate(createEvent({ x: 210, y: 210 }), 'create-space');
+      dragging.move(createEvent({ x: 210, y: 310 }));
+      dragging.end();
 
+      //then
+      elementRegistry.filter(function(element){
+        expect( !element.y || element.y >= 310 || element.y <= 210 ).toBe(true);
+      });
+    }));
+
+    iit('should move elements upwards to create some space', inject(function(dragging, elementRegistry, createSpace) {
+      //when
+      dragging.activate(createEvent({ x: 210, y: 410 }), 'create-space');
+      dragging.move(createEvent({ x: 210, y: 310 }));
+      dragging.end();
+
+      //then
+      elementRegistry.filter(function(element){
+        expect( !element.y || element.y >= 410 || element.y <= 310 ).toBe(true);
+      });
+    }));
 
   });
 
