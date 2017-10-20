3b735700dc13da7ce61f2cd9cf279e1a62ae1da8
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2017-10-20 21:47:28 +0200

    chore(tests): add headless Chrome as test runner

--- test/config/karma.unit.js
+++ test/config/karma.unit.js
@@ -9,11 +9,23 @@ var absoluteBasePath = path.resolve(path.join(__dirname, basePath));
 /* global process */
 
 // configures browsers to run test against
-// any of [ 'PhantomJS', 'Chrome', 'Firefox', 'IE']
+// any of [ 'ChromeHeadless', 'Chrome', 'Firefox', 'IE', 'PhantomJS' ]
 var browsers =
   (process.env.TEST_BROWSERS || 'PhantomJS')
     .replace(/^\s+|\s+$/, '')
-    .split(/\s*,\s*/g);
+    .split(/\s*,\s*/g)
+    .map(function(browser) {
+      if (browser === 'ChromeHeadless') {
+        process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+        // workaround https://github.com/GoogleChrome/puppeteer/issues/290
+        if (process.platform === 'linux') {
+          return 'ChromeHeadless_Linux';
+        }
+      } else {
+        return browser;
+      }
+    });
 
 
 module.exports = function(karma) {
@@ -40,6 +52,17 @@ module.exports = function(karma) {
     browsers: browsers,
 
     browserNoActivityTimeout: 30000,
+
+    customLaunchers: {
+      ChromeHeadless_Linux: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ],
+        debug: true
+      }
+    },
 
     singleRun: false,
     autoWatch: true,
