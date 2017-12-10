2571e624a639e42928b95a42fe91ffb66854bc39
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2017-12-10 12:27:04 +0100

    feat(util/Geometry): be able to pass accuracy to pointsOnLine

---
+++ test/spec/util/GeometrySpec.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var GeometryUtil = require('../../../lib/util/Geometry');
+
+var pointsOnLine = GeometryUtil.pointsOnLine;
+
+
+describe('util/Geometry', function() {
+
+  describe('#pointsOnLine', function() {
+
+    var p = { x: 0, y: 0 },
+        q = { x: 100, y: 100 },
+        z = { x: 5, y: 5 },
+        zfuzz = { x: 5, y: 10 };
+
+    it('should work', function() {
+
+      expect(pointsOnLine(p, q, z)).to.be.true;
+      expect(pointsOnLine(q, p, z)).to.be.true;
+      expect(pointsOnLine(p, z, q)).to.be.true;
+
+      // match fuzzy
+      expect(pointsOnLine(q, p, zfuzz)).to.be.true;
+
+      // match non-fuzzy
+      expect(pointsOnLine(q, p, zfuzz, 0)).to.be.false;
+    });
+
+
+    it('should be null safe', function() {
+
+      expect(pointsOnLine(p, q, null)).to.be.false;
+      expect(pointsOnLine(p, null, z)).to.be.false;
+      expect(pointsOnLine(null, q, z)).to.be.false;
+    });
+
+  });
+
+});
