64d821d9c7753a2a793aee0e4cf37417592d1285
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-10-27 00:02:28 +0200

    chore(keyboard-move-selection): simplify implementation and config
    * simplify tests
    * simplify implementation
    * expose `KeyboardMoveSelection#moveSelection` api
    
    test/spec/features/keyboard-move-selection/KeyboardMoveSelectionSpec.js

--- test/spec/features/keyboard-move-selection/KeyboardMoveSelectionSpec.js
+++ test/spec/features/keyboard-move-selection/KeyboardMoveSelectionSpec.js
@@ -1,11 +1,9 @@
 import {
   bootstrapDiagram,
-  getDiagramJS,
   inject
 } from 'test/TestHelper';
 
 import {
-  assign,
   forEach
 } from 'min-dash';
 
@@ -15,257 +13,152 @@ import keyboardMoveSelectionModule from 'lib/features/keyboard-move-selection';
 import { createKeyEvent } from 'test/util/KeyEvents';
 
 var KEYS = {
-  LEFT: [ 'ArrowLeft', 'Left' ],
-  UP: [ 'ArrowUp', 'Up' ],
-  RIGHT: [ 'ArrowRight', 'Right' ],
-  DOWN: [ 'ArrowDown', 'Down' ],
+  ArrowUp: 'up',
+  Up: 'up',
+  ArrowLeft: 'left',
+  Left: 'left',
+  ArrowRight: 'right',
+  Right: 'right',
+  ArrowDown: 'down',
+  Down: 'down'
 };
-
-var BASE_SPEED = 1;
-var HIGH_SPEED = 10;
 
 var shape1, shape2;
 
 
 describe('features/keyboard-move-selection', function() {
 
-  var defaultDiagramConfig = {
-    modules: [
-      modelingModule,
-      keyboardMoveSelectionModule
-    ],
-    canvas: {
-      deferUpdate: false
-    }
-  };
+  describe('default config', function() {
 
-  beforeEach(bootstrapDiagram(defaultDiagramConfig));
-
-  beforeEach(createShapes);
-
-  beforeEach(inject(function(selection) {
-
-    selection.select(shape1);
-    selection.select(shape2, true);
-
-  }));
+    var BASE_SPEED = 1;
+    var ACCELERATED_SPEED = 10;
 
 
-  describe('with default config', function() {
+    beforeEach(bootstrapDiagram({
+      modules: [
+        modelingModule,
+        keyboardMoveSelectionModule
+      ],
+      canvas: {
+        deferUpdate: false
+      }
+    }));
 
-    describe('without shift', function() {
-
-      /* eslint-disable no-multi-spaces */
-      var decisionTable = [
-        { desc: 'move selection left',  keys: KEYS.LEFT,  deltaX: -BASE_SPEED, deltaY: 0 },
-        { desc: 'move selection up',    keys: KEYS.UP,    deltaX: 0,           deltaY: -BASE_SPEED },
-        { desc: 'move selection right', keys: KEYS.RIGHT, deltaX: +BASE_SPEED, deltaY: 0 },
-        { desc: 'move selection down',  keys: KEYS.DOWN,  deltaX: 0,           deltaY: +BASE_SPEED },
-      ];
-      /* eslint-enable */
-
-
-      forEach(decisionTable, function(testCase) {
-
-        forEach(testCase.keys, function(key) {
-
-          it('should ' + testCase.desc, inject(function(keyboard) {
-
-            // given
-            var deltaX = testCase.deltaX,
-                deltaY = testCase.deltaY,
-                event = createKeyEvent(key, { shiftKey: false });
-
-            // when
-            keyboard._keyHandler(event);
-
-            // then
-            expect(shape1.x).to.eql(10 + deltaX);
-            expect(shape1.y).to.eql(10 + deltaY);
-            expect(shape2.x).to.eql(150 + deltaX);
-            expect(shape2.y).to.eql(10 + deltaY);
-
-          }));
-
-        });
-
-      });
-
-    });
-
-    describe('with shift', function() {
-
-      /* eslint-disable no-multi-spaces */
-      var decisionTable = [
-        { desc: 'move selection left',  keys: KEYS.LEFT,  deltaX: -HIGH_SPEED, deltaY: 0 },
-        { desc: 'move selection up',    keys: KEYS.UP,    deltaX: 0,           deltaY: -HIGH_SPEED },
-        { desc: 'move selection right', keys: KEYS.RIGHT, deltaX: +HIGH_SPEED, deltaY: 0 },
-        { desc: 'move selection down',  keys: KEYS.DOWN,  deltaX: 0,           deltaY: +HIGH_SPEED },
-      ];
-      /* eslint-enable */
+    beforeEach(inject(setupShapes));
 
 
-      forEach(decisionTable, function(testCase) {
+    describe('should default move without shift', function() {
 
-        forEach(testCase.keys, function(key) {
-
-          it('should ' + testCase.desc, inject(function(keyboard) {
-
-            // given
-            var deltaX = testCase.deltaX,
-                deltaY = testCase.deltaY,
-                event = createKeyEvent(key, { shiftKey: true });
-
-            // when
-            keyboard._keyHandler(event);
-
-            // then
-            expect(shape1.x).to.eql(10 + deltaX);
-            expect(shape1.y).to.eql(10 + deltaY);
-            expect(shape2.x).to.eql(150 + deltaX);
-            expect(shape2.y).to.eql(10 + deltaY);
-
-          }));
-
-        });
-
-      });
+      verifyMoves({ shiftKey: false }, BASE_SPEED);
 
     });
 
 
-    describe('with control or command', function() {
+    describe('should move accelerated with shift', function() {
 
-      // given
-      var TEST_KEYS = Array.prototype.concat(
-        KEYS.LEFT,
-        KEYS.UP,
-        KEYS.RIGHT,
-        KEYS.DOWN
-      );
+      verifyMoves({ shiftKey: true }, ACCELERATED_SPEED);
+
+    });
 
 
-      forEach(TEST_KEYS, function(key) {
+    describe('should not move with control', function() {
 
-        it('should not move selection when ' + key + ' and control is pressed',
-          inject(function(keyboard) {
+      verifyMove({ ctrlKey: true }, 'left', 'ArrowLeft', { x: 0, y: 0 });
 
-            // given
-            var event = createKeyEvent(key, { ctrlKey: true });
+    });
 
-            // when
-            keyboard._keyHandler(event);
 
-            // then
-            expect(shape1.x).to.eql(10);
-            expect(shape1.y).to.eql(10);
-            expect(shape2.x).to.eql(150);
-            expect(shape2.y).to.eql(10);
+    describe('should not move with meta', function() {
 
-          })
-        );
-
-      });
-
-      forEach(TEST_KEYS, function(key) {
-
-        it('should not move selection when ' + key + ' and command is pressed',
-          inject(function(keyboard) {
-
-            // given
-            var event = createKeyEvent(key, { metaKey: true });
-
-            // when
-            keyboard._keyHandler(event);
-
-            // then
-            expect(shape1.x).to.eql(10);
-            expect(shape1.y).to.eql(10);
-            expect(shape2.x).to.eql(150);
-            expect(shape2.y).to.eql(10);
-
-          })
-        );
-
-      });
+      verifyMove({ metaKey: true }, 'left', 'ArrowLeft', { x: 0, y: 0 });
 
     });
 
   });
 
 
-  describe('with custom config', function() {
+  describe('custom config', function() {
 
-    it('should use custom speed', function() {
+    var CUSTOM_SPEED = 23;
+    var CUSTOM_SPEED_ACCELERATED = 77;
 
-      // given
-      var CUSTOM_SPEED = 23;
 
-      var keyboardConfig = {
-        keyboardMoveSelection: {
-          moveSpeed: CUSTOM_SPEED
-        }
-      };
+    beforeEach(bootstrapDiagram({
+      modules: [
+        modelingModule,
+        keyboardMoveSelectionModule
+      ],
+      canvas: {
+        deferUpdate: false
+      },
+      keyboardMoveSelection: {
+        moveSpeed: CUSTOM_SPEED,
+        moveSpeedAccelerated: CUSTOM_SPEED_ACCELERATED
+      }
+    }));
 
-      bootstrapDiagram(assign(defaultDiagramConfig, keyboardConfig))();
+    beforeEach(inject(setupShapes));
 
-      var keyDownEvent = createKeyEvent(KEYS.DOWN[0]);
 
-      getDiagramJS().invoke(function(selection, keyboard) {
+    describe('should move with custom speed', function() {
 
-        // given
-        createShapes();
-        selection.select(shape1);
-        selection.select(shape2, true);
-
-        // when
-        keyboard._keyHandler(keyDownEvent);
-
-        // then
-        expect(shape1.x).to.eql(10);
-        expect(shape1.y).to.eql(10 + CUSTOM_SPEED);
-        expect(shape2.x).to.eql(150);
-        expect(shape2.y).to.eql(10 + CUSTOM_SPEED);
-
-      });
+      verifyMove({ shiftKey: false }, 'left', 'ArrowLeft', { x: -CUSTOM_SPEED, y: 0 });
 
     });
 
 
-    it('should use custom modified speed', function() {
+    describe('should move with custom accelerated speed', function() {
 
-      // given
-      var CUSTOM_HIGH_SPEED = 77;
+      verifyMove({ shiftKey: true }, 'left', 'ArrowLeft', { x: -CUSTOM_SPEED_ACCELERATED, y: 0 });
 
-      var keyboardConfig = {
-        keyboardMoveSelection: {
-          moveSpeedAccelerated: CUSTOM_HIGH_SPEED
-        }
-      };
+    });
 
-      bootstrapDiagram(assign(defaultDiagramConfig, keyboardConfig))();
+  });
 
-      var keyDownEvent = createKeyEvent(KEYS.DOWN[0], { shiftKey: true });
 
-      getDiagramJS().invoke(function(selection, keyboard) {
+  describe('api', function() {
 
-        // given
-        createShapes();
-        selection.select(shape1);
-        selection.select(shape2, true);
+    beforeEach(bootstrapDiagram({
+      modules: [
+        modelingModule,
+        keyboardMoveSelectionModule
+      ],
+      canvas: {
+        deferUpdate: false
+      }
+    }));
+
+    beforeEach(inject(setupShapes));
+
+
+    it('should provide #moveSelection(direction, false)', inject(
+      function(keyboardMoveSelection) {
 
         // when
-        keyboard._keyHandler(keyDownEvent);
+        keyboardMoveSelection.moveSelection('down', false);
 
         // then
         expect(shape1.x).to.eql(10);
-        expect(shape1.y).to.eql(10 + CUSTOM_HIGH_SPEED);
+        expect(shape1.y).to.eql(10 + 1);
         expect(shape2.x).to.eql(150);
-        expect(shape2.y).to.eql(10 + CUSTOM_HIGH_SPEED);
+        expect(shape2.y).to.eql(10 + 1);
+      }
+    ));
 
-      });
 
-    });
+    it('should provide #moveSelection(direction, true)', inject(
+      function(keyboardMoveSelection) {
+
+        // when
+        keyboardMoveSelection.moveSelection('down', true);
+
+        // then
+        expect(shape1.x).to.eql(10);
+        expect(shape1.y).to.eql(10 + 10);
+        expect(shape2.x).to.eql(150);
+        expect(shape2.y).to.eql(10 + 10);
+      }
+    ));
 
   });
 
@@ -274,26 +167,72 @@ describe('features/keyboard-move-selection', function() {
 
 // helpers ////////
 
-function createShapes() {
+function setupShapes(canvas, selection) {
 
-  getDiagramJS().invoke(function(canvas) {
+  shape1 = canvas.addShape({
+    id: 'shape1',
+    x: 10,
+    y: 10,
+    width: 100,
+    height: 100
+  });
+
+  shape2 = canvas.addShape({
+    id: 'shape2',
+    x: 150,
+    y: 10,
+    width: 100,
+    height: 100
+  });
+
+  selection.select([
+    shape1,
+    shape2
+  ]);
+}
+
+
+function delta(direction, speed) {
+
+  switch (direction) {
+  case 'left': return { x: -speed, y: 0 };
+  case 'right': return { x: speed, y: 0 };
+  case 'up': return { x: 0, y: -speed };
+  case 'down': return { x: 0, y: speed };
+  default: throw new Error('illegal direction');
+  }
+
+}
+
+
+function verifyMove(modifier, direction, key, expectedDelta) {
+
+  it('<' + key + '> --> ' + direction, inject(function(keyboard) {
 
     // given
-    shape1 = canvas.addShape({
-      id: 'shape1',
-      x: 10,
-      y: 10,
-      width: 100,
-      height: 100
-    });
+    var event = createKeyEvent(key, modifier);
 
-    shape2 = canvas.addShape({
-      id: 'shape2',
-      x: 150,
-      y: 10,
-      width: 100,
-      height: 100
-    });
+    // when
+    keyboard._keyHandler(event);
+
+    // then
+    expect(shape1.x).to.eql(10 + expectedDelta.x);
+    expect(shape1.y).to.eql(10 + expectedDelta.y);
+
+    expect(shape2.x).to.eql(150 + expectedDelta.x);
+    expect(shape2.y).to.eql(10 + expectedDelta.y);
+  }));
+
+}
+
+
+function verifyMoves(modifier, speed, expectedDelta) {
+
+  forEach(KEYS, function(direction, key) {
+
+    var d = expectedDelta || delta(direction, speed);
+
+    verifyMove(modifier, direction, key, d);
 
   });
 
