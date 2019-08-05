c17b75c535449c617c06e3d0ffa7a2ead64b4f01
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-08-05 08:02:45 +0200

    fix(dragging): compensate for missing out event
    Related to camunda/camunda-modeler#1383

--- test/spec/features/dragging/HoverFixSpec.js
+++ test/spec/features/dragging/HoverFixSpec.js
@@ -1,7 +1,11 @@
+/* global sinon */
+
 import {
   bootstrapDiagram,
   inject
 } from 'test/TestHelper';
+
+import { assign } from 'min-dash';
 
 import { createCanvasEvent as canvasEvent } from '../../../util/MockEvents';
 
@@ -12,12 +16,35 @@ describe('features/dragging - HoverFix', function() {
 
   beforeEach(bootstrapDiagram({ modules: [ dragModule ] }));
 
-  beforeEach(inject(function(canvas) {
-    canvas.addShape({ id: 'shape', x: 10, y: 10, width: 50, height: 50 });
+  var shape1,
+      shape2;
+
+  beforeEach(inject(function(canvas, elementFactory) {
+
+    shape1 = elementFactory.createShape({
+      id: 'shape1',
+      x: 10,
+      y: 10,
+      width: 50,
+      height: 50
+    });
+
+    canvas.addShape(shape1);
+
+    shape2 = elementFactory.createShape({
+      id: 'shape2',
+      x: 100,
+      y: 10,
+      width: 50,
+      height: 50
+    });
+
+    canvas.addShape(shape2);
+
   }));
 
 
-  describe('behavior', function() {
+  describe('hover fix', function() {
 
     beforeEach(inject(function(dragging) {
       dragging.setOptions({ manual: true });
@@ -44,4 +71,94 @@ describe('features/dragging - HoverFix', function() {
 
   });
 
+
+  describe('out fix', function() {
+
+    it('should ensure out', inject(function(dragging, canvas, eventBus) {
+
+      // given
+      var listener = sinon.spy(function(event) {
+        expect(event.hover).to.eql(shape1);
+        expect(event.hoverGfx).to.eql(canvas.getGraphics(shape1));
+      });
+
+      eventBus.on('drag.out', listener);
+
+      // when
+      dragging.init(canvasEvent({ x: 10, y: 10 }), 'foo');
+      eventBus.fire('element.hover', { element: shape1, gfx: canvas.getGraphics(shape1) });
+
+      // (no out)
+      eventBus.fire('element.hover', { element: shape2, gfx: canvas.getGraphics(shape2) });
+
+      // then
+      expect(listener).to.have.been.calledOnce;
+    }));
+
+
+    it('should keep event order', inject(function(dragging, canvas, eventBus) {
+
+      // given
+      var eventNames = [
+            'drag.hover',
+            'drag.out'
+          ],
+          events = recordEvents(eventNames, eventBus);
+
+      // when
+      dragging.init(canvasEvent({ x: 10, y: 10 }), 'foo');
+      eventBus.fire('element.hover', { element: shape1, gfx: canvas.getGraphics(shape1) });
+
+      // (no out)
+      eventBus.fire('element.hover', { element: shape2, gfx: canvas.getGraphics(shape2) });
+
+      // then
+      expect(events).to.eql([
+        { type: 'drag.hover', hover: shape1 },
+        { type: 'drag.out', hover: shape1 },
+        { type: 'drag.hover', hover: shape2 }
+      ]);
+    }));
+
+
+    it('should prevent additional out', inject(function(dragging, canvas, eventBus) {
+
+      // given
+      var listener = sinon.spy(function(event) {
+        expect(event.hover).to.eql(shape1);
+        expect(event.hoverGfx).to.eql(canvas.getGraphics(shape1));
+      });
+
+      eventBus.on('drag.out', listener);
+
+      // when
+      dragging.init(canvasEvent({ x: 10, y: 10 }), 'foo');
+      eventBus.fire('element.hover', { element: shape1, gfx: canvas.getGraphics(shape1) });
+      eventBus.fire('element.out', { element: shape1, gfx: canvas.getGraphics(shape1) });
+      eventBus.fire('element.hover', { element: shape2, gfx: canvas.getGraphics(shape2) });
+
+      // then
+      expect(listener).to.have.been.calledOnce;
+    }));
+
+  });
+
 });
+
+// helpers /////////////////////
+
+function recordEvents(eventNames, eventBus) {
+
+  var events = [];
+
+  eventNames.forEach(function(eventName) {
+    eventBus.on(eventName, function(e) {
+      events.push(assign({}, {
+        type: e.type,
+        hover: e.hover
+      }));
+    });
+  });
+
+  return events;
+}
