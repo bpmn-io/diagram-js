3a25679d216aec1f8cd3b480d6b82c4796ff8839
Philipp Fromme <philippfromme@outlook.com>
2019-09-06 14:50:47 +0200

    fix(keyboard-move-selection): integrate rules
    Related to bpmn-io/bpmn-js#943

---
+++ test/spec/features/keyboard-move-selection/rules/KeyboardMoveRules.js
@@ -0,0 +1,26 @@
+import inherits from 'inherits';
+
+import RuleProvider from 'lib/features/rules/RuleProvider';
+
+import { find } from 'min-dash';
+
+export default function CreateRules(injector) {
+  injector.invoke(RuleProvider, this);
+}
+
+CreateRules.$inject = [ 'injector' ];
+
+inherits(CreateRules, RuleProvider);
+
+
+CreateRules.prototype.init = function() {
+
+  this.addRule('elements.move', function(context) {
+    var shapes = context.shapes;
+
+    return !find(shapes, function(shape) {
+      return shape.id === 'shapeDisallowed';
+    });
+  });
+
+};
---
+++ test/spec/features/keyboard-move-selection/rules/index.js
@@ -0,0 +1,6 @@
+import KeyboardMoveRules from './KeyboardMoveRules';
+
+export default {
+  __init__: [ 'keyboardMoveRules' ],
+  keyboardMoveRules: [ 'type', KeyboardMoveRules ]
+};
