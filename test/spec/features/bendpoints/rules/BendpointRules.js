0528c67bbe15cc8ef1ca71c53ff93356f3682611
Philipp Fromme <philippfromme@outlook.com>
2019-11-08 11:19:47 +0100

    chore(bendpoints): add tests for bendpoint move preview
    * ensure connection preview works reversely

--- test/spec/features/bendpoints/rules/BendpointRules.js
+++ test/spec/features/bendpoints/rules/BendpointRules.js
@@ -14,14 +14,13 @@ inherits(ConnectRules, RuleProvider);
 ConnectRules.prototype.init = function() {
 
   this.addRule('connection.reconnect', function(context) {
-
     var source = context.source,
         target = context.target;
 
-    return source.type === target.type;
+    return source.type === target.type || source.type === 'C' || target.type === 'D';
   });
 
-  this.addRule('connection.updateWaypoints', function(context) {
+  this.addRule('connection.updateWaypoints', function() {
     return true;
   });
 
