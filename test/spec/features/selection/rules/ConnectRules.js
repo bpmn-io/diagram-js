fefc74538cddbbdbd47277ebadc9d6e29039324f
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-03-18 11:18:07 +0100

    fix(selection): select connect interaction target on connect
    Closes #445

---
+++ test/spec/features/selection/rules/ConnectRules.js
@@ -0,0 +1,28 @@
+import inherits from 'inherits';
+
+import RuleProvider from 'lib/features/rules/RuleProvider';
+
+export default function ConnectRules(eventBus) {
+  RuleProvider.call(this, eventBus);
+}
+
+ConnectRules.$inject = ['eventBus'];
+
+inherits(ConnectRules, RuleProvider);
+
+
+ConnectRules.prototype.init = function() {
+
+  this.addRule('connection.create', function(context) {
+    var source = context.source,
+        target = context.target;
+
+    if (source.targetOnly) {
+      return false;
+    }
+
+    if (source.notConnectable || target.notConnectable) {
+      return false;
+    }
+  });
+};
