eb0eb0db3709d08f80e5c5ce183918c0d86fcb80
Philipp Fromme <philippfromme@outlook.com>
2020-04-06 10:11:08 +0200

    chore(documentation): fix JSDoc number types

--- test/util/KeyEvents.js
+++ test/util/KeyEvents.js
@@ -6,7 +6,7 @@ import {
 /**
  * Create a fake key event for testing purposes.
  *
- * @param {String|Number} key the key or keyCode/charCode
+ * @param {String|number} key the key or keyCode/charCode
  * @param {Object} [attrs]
  * @param {string} [attrs.type]
  *
