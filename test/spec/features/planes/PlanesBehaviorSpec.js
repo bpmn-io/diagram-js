739785a431f9d017898c39b4bc8f95f9c50d869e
Martin Stamm <martin.stamm@camunda.com>
2021-11-30 14:32:31 +0100

    fix(planes): ensure we switch to existing plane

--- test/spec/features/planes/PlanesBehaviorSpec.js
+++ test/spec/features/planes/PlanesBehaviorSpec.js
@@ -51,6 +51,40 @@ describe('features/planes/PlanesBehavior', function() {
       expect(canvas.getActivePlane().name).to.equal('base');
     }));
 
+
+    it('should work after re-adding a plane', inject(function(canvas, modeling, commandStack) {
+
+      // given
+      var plane = canvas.createPlane('1');
+      canvas.setActivePlane('1');
+
+      var shape2 = canvas.addShape({
+        id: 'shape2',
+        x: 10,
+        y: 10,
+        width: 100,
+        height: 100
+      });
+      modeling.removeShape(shape2);
+
+      canvas.setActivePlane('base');
+
+      // when
+
+      // remove and re-add the plane, e.g. when removing a subprocess and undoing it
+      canvas.setRootElementForPlane(null, plane, true);
+      canvas.removePlane(plane);
+      canvas.createPlane('1');
+
+      commandStack.undo();
+
+      // then
+      var activePlane = canvas.getActivePlane();
+      expect(canvas.getRootElement()).to.exist;
+      expect(activePlane).to.not.equal(plane);
+      expect(activePlane.name).to.equal('1');
+    }));
+
   });
 
 
@@ -69,6 +103,40 @@ describe('features/planes/PlanesBehavior', function() {
 
       // then
       expect(canvas.getActivePlane().name).to.equal('base');
+    }));
+
+
+    it('should work after re-adding a plane', inject(function(canvas, modeling, commandStack) {
+
+      // given
+      var plane = canvas.createPlane('1');
+      canvas.setActivePlane('1');
+
+      var shape2 = canvas.addShape({
+        id: 'shape2',
+        x: 10,
+        y: 10,
+        width: 100,
+        height: 100
+      });
+      modeling.removeShape(shape2);
+
+      // remove and re-add the plane, e.g. when removing a subprocess and undoing it
+      canvas.setRootElementForPlane(null, plane, true);
+      canvas.removePlane(plane);
+      canvas.createPlane('1');
+      canvas.setActivePlane('base');
+
+      commandStack.undo();
+
+      // when
+      commandStack.redo();
+
+      // then
+      var activePlane = canvas.getActivePlane();
+      expect(canvas.getRootElement()).to.exist;
+      expect(activePlane).to.not.equal(plane);
+      expect(activePlane.name).to.equal('1');
     }));
 
   });
