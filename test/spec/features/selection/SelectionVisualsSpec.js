70654443c4d42dd1800c64acd7d9166b34c71040
Beatriz Mendes <bea.smendes98@gmail.com>
2022-05-17 15:09:19 +0200

    test(selectionVisuals): verify box for multi-element selection

--- test/spec/features/selection/SelectionVisualsSpec.js
+++ test/spec/features/selection/SelectionVisualsSpec.js
@@ -4,15 +4,22 @@ import {
 } from 'test/TestHelper';
 
 import selectionModule from 'lib/features/selection';
+import modelingModule from 'lib/features/modeling';
 
 import {
   query as domQuery
 } from 'min-dom';
 
+import { getBBox } from 'lib/util/Elements';
+
+import {
+  resizeBounds
+} from 'lib/features/resize/ResizeUtil';
+
 
 describe('features/selection/SelectionVisuals', function() {
 
-  beforeEach(bootstrapDiagram({ modules: [ selectionModule ] }));
+  beforeEach(bootstrapDiagram({ modules: [ selectionModule, modelingModule ] }));
 
   describe('bootstrap', function() {
 
@@ -55,19 +62,132 @@ describe('features/selection/SelectionVisuals', function() {
       canvas.addConnection(connection);
     }));
 
+    describe('single element', function() {
 
-    it('should show box on select', inject(function(selection, canvas) {
+      it('should show box on select', inject(function(selection, canvas) {
 
-      // when
-      selection.select(connection);
+        // when
+        selection.select(connection);
 
-      // then
-      var gfx = canvas.getGraphics(connection),
-          outline = domQuery('.djs-outline', gfx);
+        // then
+        var gfx = canvas.getGraphics(connection),
+            outline = domQuery('.djs-outline', gfx);
 
-      expect(outline).to.exist;
-    }));
+        expect(outline).to.exist;
+      }));
+
+    });
+
+
+    describe('multi element', function() {
+
+      var selectedShapes, outline, bounds;
+
+      beforeEach(inject(function(selection) {
+
+        selectedShapes = [ connection, shape2 ];
+        selection.select(selectedShapes);
+
+        outline = domQuery('.djs-selection-outline');
+
+        bounds = getBounds(outline);
+      }));
+
+
+      it('should show box', inject(function(selection) {
+        expect(outline).to.exist;
+      }));
+
+
+      it('selection box should contain all selected elements', inject(function(selection) {
+
+        // then
+        selectedShapes.forEach(function(shape) {
+          var bbox = getBBox(shape);
+
+          expect(bbox.x).to.be.at.least(bounds.x);
+          expect(bbox.y).to.be.at.least(bounds.y);
+          expect(bbox.x + bbox.width).to.be.at.most(bounds.x + bounds.width);
+          expect(bbox.y + bbox.height).to.be.at.most(bounds.y + bounds.height);
+        });
+
+      }));
+
+
+      it('selection box should react to element changes', inject(function(selection, modeling) {
+
+        // when
+        modeling.resizeShape(shape2, resizeBounds(bounds, 'nw', { x: 10, y: 20 }));
+
+        outline = domQuery('.djs-selection-outline');
+
+        var newBounds = getBounds(outline);
+
+        // then
+        expect(newBounds).to.not.eql(bounds);
+
+        // then
+        selectedShapes.forEach(function(shape) {
+          var bbox = getBBox(shape);
+
+          expect(bbox.x).to.be.at.least(newBounds.x);
+          expect(bbox.y).to.be.at.least(newBounds.y);
+          expect(bbox.x + bbox.width).to.be.at.most(newBounds.x + newBounds.width);
+          expect(bbox.y + bbox.height).to.be.at.most(newBounds.y + newBounds.height);
+        });
+
+      }));
+
+
+      it('selection box should react to undo/redo', inject(function(selection, modeling, commandStack) {
+
+        // given
+        modeling.resizeShape(shape2, resizeBounds(shape, 'nw', { x: 10, y: 20 }));
+
+        var outline = domQuery('.djs-selection-outline');
+
+        var bounds = getBounds(outline);
+
+        // when
+        commandStack.undo();
+
+        outline = domQuery('.djs-selection-outline');
+
+        var newBounds = getBounds(outline);
+
+        // then
+        expect(bounds).to.not.eql(newBounds);
+
+        // then
+        selectedShapes.forEach(function(shape) {
+          expectShapeToBeWithinLimits(shape, newBounds);
+        });
+
+      }));
+
+    });
 
   });
 
 });
+
+
+// helpers /////
+
+function getBounds(outline) {
+  return {
+    width: parseInt(outline.getAttribute('width')),
+    height: parseInt(outline.getAttribute('height')),
+    x: parseInt(outline.getAttribute('x')),
+    y: parseInt(outline.getAttribute('y'))
+  };
+}
+
+function expectShapeToBeWithinLimits(shape, limits) {
+  var bbox = getBBox(shape);
+
+  expect(bbox.x).to.be.at.least(limits.x);
+  expect(bbox.y).to.be.at.least(limits.y);
+  expect(bbox.x + bbox.width).to.be.at.most(limits.x + limits.width);
+  expect(bbox.y + bbox.height).to.be.at.most(limits.y + limits.height);
+}
