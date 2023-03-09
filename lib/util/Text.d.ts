70bf9e7a6abd92ac37108611116c15038d663144
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-03-09 11:49:41 +0100

    style: correct text indention

--- lib/util/Text.d.ts
+++ lib/util/Text.d.ts
@@ -1,38 +1,38 @@
 export type Dimensions = import('../util/Types').Dimensions;
 
 export type Padding = {
-    top: number;
-    left: number;
-    right: number;
-    bottom: number;
+  top: number;
+  left: number;
+  right: number;
+  bottom: number;
 };
 
 export type PaddingConfig = number | Partial<Padding>;
 
 export type Alignment = {
-    horizontal: 'center' | 'left';
-    vertical: 'top' | 'center';
+  horizontal: 'center' | 'left';
+  vertical: 'top' | 'center';
 };
 
 export type AlignmentConfig = 'center-middle' | 'center-top';
 
 export type BaseTextConfig = Partial<{
-    align: AlignmentConfig;
-    style: Record<string, any>;
-    padding: PaddingConfig;
+  align: AlignmentConfig;
+  style: Record<string, any>;
+  padding: PaddingConfig;
 }>;
 
 export type TextConfig = BaseTextConfig & Partial<{
-    size: Dimensions;
+  size: Dimensions;
 }>;
 
 export type TextLayoutConfig = BaseTextConfig & Partial<{
-    box: Dimensions;
-    fitBox: boolean;
+  box: Dimensions;
+  fitBox: boolean;
 }>;
 
 export type LineDescriptor = Dimensions & {
-    text: string;
+  text: string;
 };
 
 /**
@@ -40,28 +40,28 @@ export type LineDescriptor = Dimensions & {
  */
 export default class Text {
 
-    /**
-     * Creates a new label utility
-     *
-     * @param {TextConfig} config
-     */
-    constructor(config: TextConfig);
+  /**
+   * Creates a new label utility
+   *
+   * @param {TextConfig} config
+   */
+  constructor(config: TextConfig);
 
-    /**
-     * Returns the layouted text as an SVG element.
-     */
-    createText(text: string, options: TextLayoutConfig): SVGElement;
+  /**
+   * Returns the layouted text as an SVG element.
+   */
+  createText(text: string, options: TextLayoutConfig): SVGElement;
 
-    /**
-     * Returns a labels layouted dimensions.
-     */
-    getDimensions(text: string, options: TextLayoutConfig): Dimensions;
+  /**
+   * Returns a labels layouted dimensions.
+   */
+  getDimensions(text: string, options: TextLayoutConfig): Dimensions;
 
-    /**
-     * Creates and returns a label and its bounding box.
-     */
-    layoutText(text: string, options: TextLayoutConfig): {
-        element: SVGElement;
-        dimensions: Dimensions;
-    };
+  /**
+   * Creates and returns a label and its bounding box.
+   */
+  layoutText(text: string, options: TextLayoutConfig): {
+    element: SVGElement;
+    dimensions: Dimensions;
+  };
 }
