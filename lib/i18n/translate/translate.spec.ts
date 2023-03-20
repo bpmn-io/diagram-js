002839ea6826cb4fc157a7dd1b9ca22795c0a991
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-03-20 19:33:18 +0100

    chore(i18n): insource `translate` type

--- lib/i18n/translate/translate.spec.ts
+++ lib/i18n/translate/translate.spec.ts
@@ -1,11 +1,4 @@
-import { TranslateFunction } from './translate';
-
-const translate: TranslateFunction = (template, replacements) => {
-  for (let replacement in replacements) {
-    console.log(replacement, replacements[ replacement ]);
-  }
-
-  return template;
-};
+import translate from './translate';
 
 translate('foo {{bar}}', { bar: 'baz' });
+translate('foo {{bar}}');
