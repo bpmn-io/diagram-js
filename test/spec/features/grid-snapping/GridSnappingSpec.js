e4dc1741cc28032dc545bf4d6f861eddc4d692e9
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-03-23 10:12:44 +0100

    chore: remove internal visual grid
    Provided as external module.

--- test/spec/features/grid-snapping/GridSnappingSpec.js
+++ test/spec/features/grid-snapping/GridSnappingSpec.js
@@ -5,7 +5,6 @@ import {
 } from 'test/TestHelper';
 
 import modelingModule from 'lib/features/modeling';
-import gridModule from 'lib/features/grid-snapping/visuals';
 import gridSnappingModule from 'lib/features/grid-snapping';
 import bendpointsModule from 'lib/features/bendpoints';
 import connectModule from 'lib/features/connect';
@@ -143,7 +142,6 @@ describe('features/grid-snapping', function() {
     beforeEach(bootstrapDiagram({
       modules: [
         modelingModule,
-        gridModule,
         gridSnappingModule,
         bendpointsModule,
         connectModule,
