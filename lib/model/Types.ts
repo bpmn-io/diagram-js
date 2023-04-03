994e6cc0b3c94074315d4a91850b979d510770cb
Philipp <philippfromme@outlook.com>
2023-04-03 08:50:32 +0200

    chore: remove unused types

--- lib/model/Types.ts
+++ lib/model/Types.ts
@@ -1,39 +1,5 @@
 import { Point } from '../util/Types';
 
-export interface ModelBaseAttrs {
-  businessObject?: any;
-  id?: string;
-  label?: Label;
-  parent?: Base;
-  incoming?: Connection[];
-  outgoing?: Connection[];
-}
-
-export interface ModelAttrsShape extends ModelBaseAttrs {
-  isFrame?: boolean;
-  children?: Base[];
-  host?: Shape;
-  attachers?: Shape[];
-  x?: number;
-  y?: number;
-  width?: number;
-  height?: number;
-}
-
-export interface ModelAttrsRoot extends ModelAttrsShape {}
-
-export interface ModelAttrsLabel extends ModelAttrsShape {
-  labelTarget?: Base;
-}
-
-export interface ModelAttrsConnection extends ModelBaseAttrs {
-  source?: Base;
-  target?: Base;
-  waypoints?: Point[];
-}
-
-export type ModelAttrs = ModelAttrsConnection | ModelAttrsLabel | ModelAttrsRoot | ModelAttrsShape;
-
 export interface Base {
   businessObject: any;
   id: string;
