e7d060ca074a829dc799f44e603051cfe7294c4e
Philipp <philippfromme@outlook.com>
2023-04-12 13:23:53 +0200

    chore(palette): turn `PaletteProvider` into interface

---
+++ lib/features/palette/Palette.spec.ts
@@ -0,0 +1,44 @@
+import Diagram from '../../Diagram';
+
+import PaletteModule from '.';
+import Palette from './Palette';
+
+import { FooPaletteProvider } from './PaletteProvider.spec';
+
+const diagram = new Diagram({
+  modules: [
+    PaletteModule
+  ]
+});
+
+const palette = diagram.get<Palette>('palette');
+
+palette.open();
+
+palette.close();
+
+palette.isOpen();
+
+palette.toggle();
+
+palette.isActiveTool('foo');
+
+palette.registerProvider(new FooPaletteProvider());
+
+const entries = palette.getEntries();
+
+for (let key in entries) {
+  const entry = entries[ key ];
+
+  console.log(entry.action);
+}
+
+palette.trigger('foo', new Event('click'));
+
+palette.trigger('foo', new Event('click'), true);
+
+palette.triggerEntry('foo', 'bar', new Event('click'));
+
+palette.triggerEntry('foo', 'bar', new Event('click'), true);
+
+palette.updateToolHighlight('foo');
---
+++ lib/features/palette/PaletteProvider.spec.ts
@@ -0,0 +1,40 @@
+import PaletteProvider, { PaletteEntries, PaletteEntriesCallback } from './PaletteProvider';
+
+export class FooPaletteProvider implements PaletteProvider {
+  getPaletteEntries(): PaletteEntries {
+    return {
+      foo: {
+        action: (event, autoActivate) => {
+          console.log(event.target);
+          console.log(autoActivate);
+        },
+        className: 'foo',
+        group: 'foo',
+        html: '<marquee>Foo</marquee>',
+        imageUrl: 'data:image/svg+xml;',
+        title: 'Foo'
+      }
+    };
+  }
+};
+
+export class BarPaletteProvider implements PaletteProvider {
+  getPaletteEntries(): PaletteEntriesCallback {
+    return function(entries) {
+      return {
+        ...entries,
+        bar: {
+          action: (event, autoActivate) => {
+            console.log(event.target);
+            console.log(autoActivate);
+          },
+          className: 'bar',
+          group: 'bar',
+          html: '<marquee>Bar</marquee>',
+          imageUrl: 'data:image/svg+xml;',
+          title: 'Bar'
+        }
+      };
+    };
+  }
+};
