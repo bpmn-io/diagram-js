c8b07115b890b0c08028d9721c2f8a3c16cb4982
Philipp <philippfromme@outlook.com>
2023-04-24 10:03:17 +0200

    fix: fix types test

--- lib/draw/DefaultRenderer.spec.ts
+++ lib/draw/DefaultRenderer.spec.ts
@@ -1,9 +1,31 @@
 import Diagram from "../Diagram";
+
 import DefaultRenderer from "./DefaultRenderer";
+
+import ElementFactory from "../core/ElementFactory";
+import GraphicsFactory from "../core/GraphicsFactory";
 
 const diagram = new Diagram();
 
 const defaultRenderer = diagram.get<DefaultRenderer>('defaultRenderer');
 
-defaultRenderer.canRender(null);
-defaultRenderer.drawShape(null, null);
+const elementFactory = diagram.get<ElementFactory>('elementFactory'),
+      graphicsFactory = diagram.get<GraphicsFactory>('graphicsFactory');
+
+const shape = elementFactory.createShape(),
+      shapeGfx = graphicsFactory.create('shape', shape);
+
+const connection = elementFactory.createConnection(),
+      connectionGfx = graphicsFactory.create('connection', connection);
+
+defaultRenderer.canRender(shape);
+
+defaultRenderer.canRender(connection);
+
+defaultRenderer.drawShape(shapeGfx, shape);
+
+defaultRenderer.drawConnection(connectionGfx, connection);
+
+defaultRenderer.getShapePath(shape);
+
+defaultRenderer.getConnectionPath(connection);
