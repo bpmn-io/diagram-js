e317f46d76ea4b50d8c56ec036c1e6aeb83206e0
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-08-11 00:02:14 +0200

    feat(copy-paste): decouple from create

--- test/spec/features/copy-paste/CopyPasteSpec.js
+++ test/spec/features/copy-paste/CopyPasteSpec.js
@@ -16,6 +16,7 @@ import {
 
 import attachSupportModule from 'lib/features/attach-support';
 import copyPasteModule from 'lib/features/copy-paste';
+import createModule from 'lib/features/create';
 import modelingModule from 'lib/features/modeling';
 import rulesModule from './rules';
 import selectionModule from 'lib/features/selection';
@@ -39,6 +40,7 @@ describe('features/copy-paste', function() {
     modules: [
       attachSupportModule,
       copyPasteModule,
+      createModule,
       modelingModule,
       rulesModule,
       selectionModule
