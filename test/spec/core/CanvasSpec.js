cefd9e1af672fa88eb0917019f38ace0a572cc84
Philipp Fromme <philipp.fromme@camunda.com>
2024-09-23 13:17:54 +0200

    fix(canvas): return copy of viewbox
    Returning reference to viewbox can lead to undesired effects.

--- test/spec/core/CanvasSpec.js
+++ test/spec/core/CanvasSpec.js
@@ -867,6 +867,25 @@ describe('Canvas', function() {
       }));
 
 
+      it('should return copy of viewbox', inject(function(canvas) {
+
+        // given
+        canvas.addShape({ id: 's0', x: 0, y: 0, width: 300, height: 300 });
+
+        var shape = canvas.addShape({ id: 's1', x: 10000, y: 0, width: 300, height: 300 });
+
+        var viewbox = canvas.viewbox(),
+            viewboxStringified = JSON.stringify(viewbox);
+
+        // when
+        canvas.scrollToElement(shape);
+
+        // then
+        expect(JSON.stringify(viewbox)).to.eql(viewboxStringified);
+        expect(JSON.stringify(canvas.viewbox())).not.to.eql(viewboxStringified);
+      }));
+
+
       it('should provide default viewbox / overflowing diagram', inject(function(canvas) {
 
         // given
