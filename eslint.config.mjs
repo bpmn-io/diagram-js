12a43d39e32821ca9056b67d7a8b6044ca556361
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-10-10 12:12:50 +0200

    chore: adjust eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,49 @@
 import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
 
 export default [
-  ...bpmnIoPlugin.configs.browser.map((config) => {
+  ...bpmnIoPlugin.configs.node.map(config => {
+
     return {
       ...config,
-      files: [ 'lib/**/*.js' ]
+      files: [
+        '*.js',
+        '*.mjs'
+      ]
     };
   }),
-  ...bpmnIoPlugin.configs.mocha.map((config) => {
+  ...bpmnIoPlugin.configs.browser.map(config => {
+
     return {
       ...config,
-      files: [ 'test/**/*.js' ]
+      files: [
+        'lib/**/*.js'
+      ]
     };
-  })
+  }),
+  ...bpmnIoPlugin.configs.mocha.map(config => {
+
+    return {
+      ...config,
+      files: [
+        '**/test/**/*.js'
+      ]
+    };
+  }),
+  {
+    languageOptions: {
+      globals: {
+        sinon: true
+      }
+    },
+    rules: {
+      'mocha/no-setup-in-describe': 'off',
+      'mocha/no-sibling-hooks': 'off',
+      'mocha/max-top-level-suites': 'off',
+      'mocha/no-pending-tests': 'off',
+      'mocha/no-skipped-tests': 'off'
+    },
+    files: [
+      '**/test/**/*.js'
+    ]
+  }
 ];
